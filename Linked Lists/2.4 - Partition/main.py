bb9ac3723c5912fbeea44f560ae5e2d3469963f1
Matthew Bunce <mbunce@uoguelph.ca>
2022-10-02 15:47:07 -0400

    partition

--- Linked Lists/2.4 - Partition/main.py
+++ Linked Lists/2.4 - Partition/main.py
@@ -1,7 +1,7 @@
+from cgitb import small
+
+
 class Node(object):
-    """
-    This is a Node class.
-    """
 
     def __init__(self, value):
         self.value = value
@@ -14,10 +14,7 @@ class LinkedList(object):
         self.head = head
 
     def insert_node(self, data):
-        """
-        :param data: Node data
-        :return: None
-        """
+
         node = Node(data)
         node.next = self.head
         self.head = node
@@ -30,13 +27,82 @@ class LinkedList(object):
             current = current.next
 
 
+    def partition(self, x):
+
+        current = self.head
+
+        smallerHead = None
+        smallerLast = None
+        greaterLast = None
+        greaterHead = None
+        equalHead = None
+        equalLast = None
+
+        while(current != None):
+
+            if(current.value == x):
+
+                if(equalHead == None):
+                    equalHead = equalLast = current
+                else:
+                    equalLast.next = current
+                    equalLast = equalLast.next
+
+            elif(current.value < x):
+                
+                if(smallerHead == None):
+                    smallerLast = smallerHead = current
+                else:
+                    smallerLast.next = current
+                    smallerLast = current
+            
+            else:
+                if (greaterHead == None):
+                    greaterLast = greaterHead = current
+                else:
+                    greaterLast.next = current
+                    greaterLast = current
+            
+            current = current.next
+                
+        if (greaterLast != None) :
+            greaterLast.next = None
+    
+        # Connect three lists
+    
+        # If smaller list is empty
+        if (smallerHead == None) :
+        
+            if (equalHead == None) :
+                self.head = greaterHead
+            equalLast.next = greaterHead
+            self.head = equalHead
+        
+        # If smaller list is not empty
+        # and equal list is empty
+        if (equalHead == None) :
+        
+            smallerLast.next = greaterHead
+            self.head = smallerHead
+        
+        # If both smaller and equal list
+        # are non-empty
+        smallerLast.next = equalHead
+        equalLast.next = greaterHead
+        self.head = smallerHead
+
 
 my_list = LinkedList()
 my_list.insert_node(3)
+my_list.insert_node(2)
 my_list.insert_node(5)
-my_list.insert_node(15)
-my_list.insert_node(32)
-my_list.insert_node(25)
-my_list.insert_node(80)
-my_list.insert_node(56)
+my_list.insert_node(2)
+my_list.insert_node(3)
+my_list.insert_node(4)
+my_list.insert_node(1)
+my_list.display_list()
+
+print("\nPartition:\n")
+my_list.partition(3)
+
 my_list.display_list()
