55d191f0e407cd59eef9faccdcd928b8e905a5c7
Matthew Bunce <mbunce@uoguelph.ca>
2022-10-06 19:52:38 -0400

    starting animal shelter

---
+++ Stacks and Queues/3.6 - Animal Shelter/main.py
@@ -0,0 +1,11 @@
+class AnimalShelter(object):
+    def __init__(self):
+        pass
+    def enqueue(self, name, type):
+        pass
+    def dequeueAny(self):
+        pass
+    def dequeueDog(self):
+        pass
+    def dequeueCat(self):
+        pass
