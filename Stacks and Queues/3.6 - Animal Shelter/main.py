ccacfd9632efa1ad1ede8577db0ce957575cfaa4
Matthew Bunce <mbunce@uoguelph.ca>
2022-10-09 12:32:58 -0400

    3.6

--- Stacks and Queues/3.6 - Animal Shelter/main.py
+++ Stacks and Queues/3.6 - Animal Shelter/main.py
@@ -1,11 +1,56 @@
 class AnimalShelter(object):
+
     def __init__(self):
-        pass
+        self.dogs = []
+        self.cats = []
+        self.place_in_line = 0
+
     def enqueue(self, name, type):
-        pass
-    def dequeueAny(self):
-        pass
+        if type == "DOG":
+            self.place_in_line += 1
+            self.dogs.append([name , self.place_in_line])
+
+        elif type == "CAT":
+            self.place_in_line += 1           
+            self.cats.append([name , self.place_in_line])
+
+        else:
+            print("Only dogs and cats my guy")
+
+
+    def dequeueOldest(self):
+        oldDog = self.dogs[0][1]
+        oldCat = self.cats[0][1]
+
+        if oldCat < oldDog:
+            self.cats.pop(0)
+        else:
+            self.dogs.pop(0)
+
     def dequeueDog(self):
-        pass
+        self.dogs.pop(0)
+
     def dequeueCat(self):
-        pass
+        self.cats.pop(0)
+
+    def display(self):
+        print("Dogs:")
+        print(self.dogs)
+
+        print("Cats:")
+        print(self.cats)
+
+#Main Driver Code
+myShelter = AnimalShelter()
+myShelter.enqueue("spot", "CAT")
+myShelter.enqueue("mittens", "CAT")
+myShelter.enqueue("Fluffers", "CAT")
+myShelter.enqueue("Duke", "DOG")
+myShelter.enqueue("Buddy", "DOG")
+myShelter.enqueue("Barker", "DOG")
+myShelter.display()
+myShelter.dequeueCat()
+myShelter.dequeueDog()
+myShelter.display()
+myShelter.dequeueOldest()
+myShelter.display()
