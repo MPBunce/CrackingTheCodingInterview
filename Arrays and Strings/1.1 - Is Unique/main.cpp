8480aa85538e1ab02975a40463ba57d102a73d17
Matthew Bunce <mbunce@uoguelph.ca>
2022-10-10 15:58:44 -0400

    cpp

--- Arrays and Strings/1.1 - Is Unique/main.cpp
+++ Arrays and Strings/1.1 - Is Unique/main.cpp
@@ -1,22 +1,52 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 using namespace std;
 
 //Declaring Function
-bool isUnique();
+bool isUnique(string inputString);
+bool isUniqueTwo(string inputString);
 
 int main() {
 
-    string testString = "Test";
-    if(isUnique()){
-
+    string testString = "Teeeest";
+    if(isUniqueTwo(testString)){
+        std::cout << "\n" <<testString << " is a unqiue string!\n";
     } else {
-
+        std::cout << "\n" << testString << " is not a unqiue string :(\n";
     }
 
     return 0;
 }
 
-bool isUnique(){
-    return false;
+//this is faster doe
+bool isUnique(string inputString){
+
+    unordered_map<char, int> table;
+    string used = (inputString);
+    for(int i = 0; i < used.size(); ++i){
+
+        table[ used[i] ]++;
+
+        if(table[ used[i] ] > 1){
+            return false;
+        }
+
+    }
+    return true;
+}
+
+//using no additional data scructures
+bool isUniqueTwo(string inputString){
+
+    for(int i = 0; i < inputString.size(); ++i){
+        for(int j = i + 1; j < inputString.size(); ++j){
+
+            if(inputString[i] == inputString[j]){
+                return false;
+            }
+
+        }
+    }
+    return true;
 }
