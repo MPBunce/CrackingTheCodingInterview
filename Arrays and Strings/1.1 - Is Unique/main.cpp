29f413eeca3f8205024ab857fafb5d16693d8bcf
Matthew Bunce <mbunce@uoguelph.ca>
2022-10-10 15:27:26 -0400

    cpp

---
+++ Arrays and Strings/1.1 - Is Unique/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Declaring Function
+bool isUnique();
+
+int main() {
+
+    string testString = "Test";
+    if(isUnique()){
+
+    } else {
+
+    }
+
+    return 0;
+}
+
+bool isUnique(){
+    return false;
+}
