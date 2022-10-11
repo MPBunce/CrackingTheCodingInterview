f6795a0b5544e681f1ab8e731ba9afc3f77c899d
Matthew Bunce <m@Matthews-MacBook-Pro.local>
2022-10-11 18:02:26 -0400

    RUST

--- Arrays and Strings/1.1 - Is Unique/Rust/src/main.rs
+++ Arrays and Strings/1.1 - Is Unique/Rust/src/main.rs
@@ -1,5 +1,35 @@
 #![allow(non_snake_case)]
 
 fn main() {
-    println!("Hello, world!");
+    let testString = String::from("apples");
+
+    if isStringUniqueTwoLoops(&testString) {
+        println!("{}, This is a unique string!\n", &testString)
+    } else {
+        println!("{}, This is not a unique string :(\n", &testString)
+    }
 }
+
+fn isStringUniqueTwoLoops(msg: &String) -> bool{
+
+    //Cannot iterate over a string in Rust convert to char vector XD
+    let my_vec: Vec<char> = msg.chars().collect();
+    println!("THE STRING : {}", msg);
+
+    for i in 0..my_vec.len() {
+        for j in i..my_vec.len() {
+
+            if my_vec[i] == my_vec[j] {
+                return false;
+            }
+
+        }
+    }
+
+    return true;
+}
+
+// fn isStringUniqueHashMap(msg: &String) -> bool{
+//     println!("THE STRING : {}", msg);
+//     return true;
+// }
